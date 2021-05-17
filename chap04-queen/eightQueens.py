a85e6c22ea46b9923c43c88786dc668b66a039f4
tomoyk <tmyk.kym@gmail.com>
2021-05-17 15:10:06 +0900

    add 8queens.py

---
+++ chap04-queen/eightQueens.py
@@ -0,0 +1,35 @@
+from noConflicts import noConflicts
+
+def EightQueens(n=8):
+    board = [-1] * n
+    for i in range(n):
+        board[0] = i
+        for j in range(n):
+            board[1] = j
+            if not noConflicts(board, 1):
+                continue
+            for k in range(n):
+                board[2] = k
+                if not noConflicts(board, 2):
+                    continue
+                for l in range(n):
+                    board[3] = l
+                    if not noConflicts(board, 3):
+                        continue
+                    for m in range(n):
+                        board[4] = m
+                        if not noConflicts(board, 4):
+                            continue
+                        for o in range(n):
+                            board[5] = o
+                            if not noConflicts(board, 5):
+                                continue
+                            for p in range(n):
+                                board[6] = p
+                                if not noConflicts(board, 6):
+                                    continue
+                                for q in range(n):
+                                    board[7] = q
+                                    if noConflicts(board, 7):
+                                        print(board)
+    return
